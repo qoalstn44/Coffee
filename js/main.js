c275994f5d46182e7d503d4ae9ac51eeeb1f53ca
{배민수} <qoalstn44@naver.com>
2022-06-16 16:11:58 +0900

    a

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // 검색창 요서(.search) 찾기.
 const searchEl = document.querySelector('.search');
 const searchElnputEl = searchEl.querySelector('input');
+//const searchMainMenu = document.querySelector('main-menu');
 //검색할 요소를 클릭하면 실행.
 searchEl.addEventListener('click', function () {
     searchElnputEl.focus();
