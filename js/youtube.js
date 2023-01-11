d8b50af271a62d0fccea4346fa385f92a6a573c0
qoalstn44 <qoalstn44@naver.com>
2023-01-11 20:02:29 +0900

    💄Feat: 파일수정
    by @qoalstn44

--- js/youtube.js
+++ js/youtube.js
@@ -1,19 +1,19 @@
-var tag = document.createElement('script');
+var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 function onYouTubePlayerAPIReady() {
-  new YT.Player('player', {
-    videoId: 'Tdwetb0Mvyc',
+  new YT.Player("player", {
+    videoId: "WT9rV7rZniE",
     playerVars: {
       autoplay: true,
       loop: true,
-      playlist: 'Tdwetb0Mvyc'
+      playlist: "WT9rV7rZniE",
     },
     events: {
-        onReady: function (event) {
-            event.target.mute()
-        }
-    }
-  })
+      onReady: function (event) {
+        event.target.mute();
+      },
+    },
+  });
 }
